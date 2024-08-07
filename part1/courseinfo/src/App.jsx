4e7b6e07b92547fa823f8010b4232aeb73b185f2
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-08 01:32:19 +0300

    Complete exercise 1.9

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,19 @@
 import { useState } from 'react';
 
-const Statistics = ({ good, neutral, bad, all }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good + bad * -1) / all;
+  const positive = 100 * good / all;
+
+  if (all === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Statistics</h1>
@@ -8,8 +21,8 @@ const Statistics = ({ good, neutral, bad, all }) => {
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
       <div>all {all}</div>
-      <div>average {(good + bad * -1) / all}</div>
-      <div>positive {100 * good / all} %</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   );
 };
@@ -18,19 +31,15 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
-    setAll(all + 1);
   };
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
-    setAll(all + 1);
   };
   const handleBadClick = () => {
     setBad(bad + 1);
-    setAll(all + 1);
   };
 
   return (
@@ -41,11 +50,10 @@ const App = () => {
         <button onClick={handleNeutralClick}>neutral</button>
         <button onClick={handleBadClick}>bad</button>
       </div>
-      <Statistics 
-        good={good} 
-        neutral={neutral} 
+      <Statistics
+        good={good}
+        neutral={neutral}
         bad={bad}
-        all={all}
       />
     </div>
   );
