011fe02096aaacd4d5bf13694328f2edeb2d8f7e
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-08 01:00:35 +0300

    Add total click number display

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,20 +4,35 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAll(all + 1);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
+  };
 
   return (
     <div>
       <div>
         <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
       </div>
       <div>
         <h1>statistics</h1>
         <div>good {good}</div>
         <div>neutral {neutral}</div>
         <div>bad {bad}</div>
+        <div>all {all}</div>
       </div>
     </div>
   );
