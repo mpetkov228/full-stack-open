ed4d9d2c3053bbed8124b5af0649b3861474bb1f
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-07 23:06:22 +0300

    Complete exercise 1.6

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -9,15 +9,15 @@ const App = () => {
     <div>
       <div>
         <h1>give feedback</h1>
-        <button>good</button>
-        <button>neutral</button>
-        <button>bad</button>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
       <div>
         <h1>statistics</h1>
-        <p>good</p>
-        <p>neutral</p>
-        <p>bad</p>
+        <div>good {good}</div>
+        <div>neutral {neutral}</div>
+        <div>bad {bad}</div>
       </div>
     </div>
   );
