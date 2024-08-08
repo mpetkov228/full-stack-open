c598848af7c82b86e35866041622f9a746b6ece2
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-09 00:13:54 +0300

    Complete exercise 2.2 and 2.3*

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,6 +1,6 @@
 const Header = ({ name }) => <h1>{name}</h1>;
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ sum }) => <p><strong>total of {sum} exercises</strong></p>;
 
 const Part = ({ name, exercises }) => 
   <p>
@@ -14,15 +14,18 @@ const Content = ({ parts }) =>
         key={part.id}
         name={part.name}
         exercises={part.exercises}
-      />)
-    }
+      />
+    )}
   </>;
 
 const Course = ({ course }) => {
+  const sum = course.parts.reduce((acc, part) => acc + part.exercises, 0);
+
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total sum={sum} />
     </div>
   );
 };
