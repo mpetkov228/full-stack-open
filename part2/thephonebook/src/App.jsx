d60a4550b79822088e321a51bbc10faf60fbb7ba
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-11 00:39:35 +0300

    Complete exercise 2.6

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 1 }
+    { name: 'Arto Hellas' }
   ]);
   const [newName, setNewName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  };
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
       <div>
         <h2>Phonebook</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            name: <input />
+            name: <input value={newName} onChange={handleChange} />
           </div>
           <div>
             <button type="submit">add</button>
@@ -19,7 +33,7 @@ const App = () => {
         </form>
         <h2>Numbers</h2>
         {persons.map(person => {
-          return <div key={person.id}>{person.name}</div>
+          return <div key={person.name}>{person.name}</div>
         })}
       </div>
   );
