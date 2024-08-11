a585ce17c14536ab62f617d8427ff909ef682253
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-11 11:34:30 +0300

    Complete exercise 2.9*

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -2,19 +2,24 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const personsToShow = showAll ? persons : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()));
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const newPerson = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     };
 
     const found = persons.find((person) => person.name === newPerson.name);
@@ -34,26 +39,32 @@ const App = () => {
   const onNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const onSearchChange = (event) => {
+    setSearch(event.target.value);
+    setShowAll(false);
+  };
 
   return (
+    <div>
+      <h2>Phonebook</h2>
       <div>
-        <h2>Phonebook</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            name: <input value={newName} onChange={onNameChange} />
-          </div>
-          <div>
-            number: <input value={newNumber} onChange={onNumberChange}/>
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-        </form>
-        <h2>Numbers</h2>
-        {persons.map(person => {
-          return <div key={person.name}>{person.name} {person.number}</div>
-        })}
+        filter shown with <input value={search} onChange={onSearchChange} />
       </div>
+      <h2>add a new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={onNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={onNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {personsToShow.map(person => <div key={person.id}>{person.name} {person.number}</div>)}
+    </div>
   );
 };
 
